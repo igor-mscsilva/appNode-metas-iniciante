0df2bb833e648cb41fdcf4ac0bccd9cf639eb6a4
Igor Silva <igor_mscsilva@outlook.com>
2024-09-10 00:26:31 -0300

    fundamentos 003

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
-// hello world
-console.log("Hello World!");
+
+
+let meta = {
+    value: "Ler um livro por mes",
+    checked: false,
+    log: (info) => {
+        console.log(info)
+    }
+}
+
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked: false
+    }
+]
+
+meta.log(metas[1].value)
+
+// function // arrow function
+
+//const criarMeta = () => {}
+
+//function CriarMeta () {}
--- todo.md
+++ todo.md
@@ -5,3 +5,7 @@
 - Marcar/Desmarcar metas realizadas
 - Remover metas
 - Sistema de mensagem
+
+Array -let metas = ["Mayk", "alo"]
+
+Object - let metas = {}
